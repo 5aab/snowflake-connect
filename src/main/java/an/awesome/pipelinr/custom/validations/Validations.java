6eaabeb86573066e9b772c60e17c4f71095071c0
Ish Mahajan <ishmahajanbhanu@gmail.com>
2021-10-05 15:53:43 +0530

    initial commit

---
+++ src/main/java/an/awesome/pipelinr/custom/validations/Validations.java
@@ -0,0 +1,25 @@
+package an.awesome.pipelinr.custom.validations;
+
+import an.awesome.pipelinr.Command;
+import lombok.AllArgsConstructor;
+import org.springframework.core.annotation.Order;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import java.util.Set;
+
+@Order(4)
+@AllArgsConstructor
+public class Validations implements Command.Middleware {
+
+    private Validator validator;
+
+    @Override
+    public <R, C extends Command<R>> R invoke(C command, Next<R> next) {
+        Set<ConstraintViolation<C>> validations = validator.validate(command);
+        if(!validations.isEmpty()){
+            throw new CommandValidationException(validations);
+        }
+        return next.invoke();
+    }
+}
