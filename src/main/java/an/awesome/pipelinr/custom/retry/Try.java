a4dbb66f4ac67dc977b7e7e8c555ade1d34aa694
Ish Mahajan <ishmahajanbhanu@gmail.com>
2021-10-05 15:36:45 +0530

    initial commit

--- src/main/java/an/awesome/pipelinr/custom/retry/Try.java
+++ src/main/java/an/awesome/pipelinr/custom/retry/Try.java
@@ -3,6 +3,9 @@ package an.awesome.pipelinr.custom.retry;
 import an.awesome.pipelinr.Command;
 import lombok.AllArgsConstructor;
 import org.springframework.core.annotation.Order;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 
 @Order(2)
 @AllArgsConstructor
@@ -12,6 +15,15 @@ public class Try<R, C extends Command<R>> implements Command.Middleware {
 
     @Override
     public <R, C extends Command<R>> R invoke(C command, Next<R> next) {
-        return null;
+        RetryTemplate retryTemplate = new RetryTemplate();
+        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+        fixedBackOffPolicy.setBackOffPeriod(2000l);
+
+        SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy();
+        simpleRetryPolicy.setMaxAttempts(3);
+
+        retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+        retryTemplate.setRetryPolicy(simpleRetryPolicy);
+        return retryTemplate.execute(arg -> next.invoke());
     }
 }
