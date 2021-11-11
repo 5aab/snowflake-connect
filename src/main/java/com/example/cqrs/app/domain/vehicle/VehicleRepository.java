7fcaf98b20003b55f6a510bcc3823ca518cf31f4
Ish Mahajan <ishmahajanbhanu@gmail.com>
2021-11-12 01:49:07 +0530

    query dsl extension dynamic

--- src/main/java/com/example/cqrs/app/domain/vehicle/VehicleRepository.java
+++ src/main/java/com/example/cqrs/app/domain/vehicle/VehicleRepository.java
@@ -1,5 +1,6 @@
 package com.example.cqrs.app.domain.vehicle;
 
+import com.example.cqrs.event.QDomainAggregateRoot;
 import com.example.cqrs.persistence.CustomRepository;
 import com.querydsl.core.types.Path;
 import org.bitbucket.gt_tech.spring.data.querydsl.value.operators.ExpressionProviderFactory;
@@ -7,7 +8,12 @@ import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.stereotype.Repository;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Repository
 public interface VehicleRepository extends CustomRepository<Vehicle, Integer>, QuerydslBinderCustomizer<QVehicle> {
@@ -19,10 +25,20 @@ public interface VehicleRepository extends CustomRepository<Vehicle, Integer>, Q
 
     @Override
     default void customize(QuerydslBindings bindings, QVehicle root) {
+        Field[] declaredFields = root.getClass().getDeclaredFields();
+        List<Path> paths = Arrays.stream(declaredFields).map(f -> {
+                    try {
+                        return (Path) f.get(root);
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                }).filter(Objects::nonNull)
+                .filter(p -> !(p.getClass().equals(QDomainAggregateRoot.class) || p.getClass().equals(root.getClass())))
+                .collect(Collectors.toList());
 
-        createBinding(bindings, root.id);
-        createBinding(bindings, root.soundSystem);
-        createBinding(bindings, root.brandName);
+        paths.forEach(rootPath -> createBinding(bindings, rootPath));
+
     }
 
     private void createBinding(QuerydslBindings bindings, Path rootPath) {
